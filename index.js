aa86127c15018bb45514949cd46ca8e2a73be7dd
Frostzeichen <cruztjomar@gmail.com>
2023-11-30 18:39:47 +0800

    Feature: add new reading cycle count

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import { request } from "./controllers/sensorRequests.js";
 import { delay } from "./controllers/time.js";
+import { addNewReading, getAll } from "./controllers/db.js";
 import "dotenv/config"
 
 const addresses = [process.env.MCU_1]
 
 const main = async () => {
     let dataCache = [];
+    let cycles = 0;
+
     for (;;) {
         for (let i = 0; i < addresses.length; i++) {
-            request(addresses[i]).then((data) => dataCache.push(data));
+            request(addresses[i]).then((data) => {
+                data["timestamp"] = new Date();
+                return dataCache.push(data)
+            });
         }
-        console.log(dataCache);
-        
+
+        if (cycles == 3) { // One cycle is designed for roughly 1 second
+            console.log("Saving...")
+            addNewReading(dataCache);
+            dataCache = [];
+            getAll();
+            cycles = 0;
+            console.log("Done!");
+        } else cycles++;
+
+        console.log(`Cycles: ${cycles}`);
         await delay(process.env.INTERVAL);
     }
 }
